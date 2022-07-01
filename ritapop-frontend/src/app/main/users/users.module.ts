c0082a5513269c69c56437932d5b8ae31e1659eb
tomascarralero <tcarralero@gmail.com>
2022-07-01 14:46:23 +0200

    Create users component and Add menu option

--- ritapop-frontend/src/app/main/users/users.module.ts
+++ ritapop-frontend/src/app/main/users/users.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { UsersHomeComponent } from './users-home/users-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UsersHomeComponent],
   imports: [
     CommonModule,
     OntimizeWebModule,
--- ritapop-frontend/src/app/shared/app.menu.config.ts
+++ ritapop-frontend/src/app/shared/app.menu.config.ts
@@ -2,5 +2,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
+  { id: 'users', name: 'USERS', icon: 'people', route: '/main/users' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
